16c8a2daefd1eefe963f800168047a5f2dc65c26
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2023-01-22 14:21:21 +0100

    Update cart.js

--- models/cart.js
+++ models/cart.js
@@ -1,18 +1,41 @@
 const mongoose = require('mongoose');
 
 const CartSchema = new mongoose.Schema({
-    duration: { type: String, required: true, trim: true },
+    duration: { type: Number, required: true, default: 12 },
     category: { type: String, },
     user_id: {
         type: mongoose.Types.ObjectId,
         ref: "users"
     },
-    package: {
+    package_id: {
         type: mongoose.Types.ObjectId,
         ref: "packages"
     },
+    product_id: [{
+        item: {
+            type: mongoose.Types.ObjectId,
+            ref: "products"
+        },
+        qty: { type: Number, default: 1 }
+    }],
     frequency: {
-        type: { String, required: true }
+        type: String, required: true,
+        enum: ["daily", "weekly", "monthly"]
+    },
+    total: {
+        type: Number, required: true
+    },
+    daily: {
+        type: Number
+    },
+    weekly: {
+        type: Number
+    },
+    monthly: {
+        type: Number
+    },
+    status: {
+        type: String, default: "active"
     }
 }, {
     toJSON: {
