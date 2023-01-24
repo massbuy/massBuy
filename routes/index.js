01924f768c766fa20cd316579a7300ab3fbdb407
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2023-01-24 12:23:22 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ require('./endpoints/User')(app);
 require('./endpoints/Product')(app);
 require('./endpoints/Packages')(app);
 require('./endpoints/Category')(app);
+require('./endpoints/Payment')(app);
 require('./endpoints/PackageCategory')(app);
 
 module.exports = app;
